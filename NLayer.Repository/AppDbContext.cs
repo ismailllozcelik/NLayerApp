f82de736ecc5756f6fed7a05be32916be76522e8
İsmail <ismailll.ozcelik@gmail.com>
2023-08-30 10:57:04 +0300

    AppDbContext Repository katmanına eklendi

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        //product feature aslında products üzerinden doldurulmalı
+        // öğrenmek amaçlı açık tutuyoruz
+        public DbSet<ProductFeature> productFeatures { get; set; }
+    }
+}
